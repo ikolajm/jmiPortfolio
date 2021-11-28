626de17f0d7b59b43ee4854c67d0b0db7e22dc3d
JMI <ikolajm@gmail.com>
2021-11-28 18:11:42 -0500

    added video of tellem

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,14 @@
 const toggleProjectA = () => {
+    const projectA = document.querySelector('#project-a')
     const button = document.querySelector('#project__button-a');
     const card = document.querySelector('#project__card-a');
+    const projectAImage = projectA.firstElementChild
     if (!card.classList.contains('visible')) {
         // Turn the button 45deg
         button.classList.toggle('turned');
         setTimeout(() => {
+            // Turn brightness of image/video down
+            projectAImage.classList.toggle('brightness-down');
             // Pull the button up/down
             button.classList.toggle('pulled');
         }, 300)
@@ -16,6 +20,8 @@ const toggleProjectA = () => {
         // Turn the button 45deg
         button.classList.toggle('turned');
         setTimeout(() => {
+            // Turn brightness of image/video down
+            projectAImage.classList.toggle('brightness-down');
             // Pull card up to top-20%
             card.classList.toggle('visible')
             // Pull the button up/down
@@ -25,12 +31,16 @@ const toggleProjectA = () => {
 }
 
 const toggleProjectB = () => {
+    const projectB = document.querySelector('#project-b')
     const button = document.querySelector('#project__button-b');
     const card = document.querySelector('#project__card-b');
+    const projectBImage = projectB.firstElementChild
     if (!card.classList.contains('visible')) {
         // Turn the button 45deg
         button.classList.toggle('turned');
         setTimeout(() => {
+            // Turn brightness of image/video down
+            projectBImage.classList.toggle('brightness-down');
             // Pull the button up/down
             button.classList.toggle('pulled');
         }, 300)
@@ -42,6 +52,8 @@ const toggleProjectB = () => {
         // Turn the button 45deg
         button.classList.toggle('turned');
         setTimeout(() => {
+            // Turn brightness of image/video down
+            projectBImage.classList.toggle('brightness-down');
             // Pull card up to top-20%
             card.classList.toggle('visible')
             // Pull the button up/down
@@ -49,3 +61,23 @@ const toggleProjectB = () => {
         }, 300)
     }
 }
+
+// const toggleTellemPlay = () => {
+//     const tellemVideo = document.querySelector("#tellem-video");
+//     const videoToggle = document.querySelector('#video-toggle');
+//     console.log(videoToggle.children)
+//     const playBtn = videoToggle.children[0]
+//     const pauseBtn = videoToggle.children[1]
+
+//     if (tellemVideo.classList.contains('playing')) {
+//         tellemVideo.pause()
+//         tellemVideo.classList.remove('playing')
+//         pauseBtn.classList.remove('show')
+//         playBtn.classList.add('show')
+//     } else {
+//         tellemVideo.play()
+//         playBtn.classList.remove('show')
+//         pauseBtn.classList.add('show')
+//         tellemVideo.classList.add('playing')
+//     }
+// }
